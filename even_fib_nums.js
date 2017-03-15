cff622432e0ecbe939badaf75553e2f1b247b08b
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-15 09:55:23 -1000

    With corrections and an array solution

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -14,8 +14,32 @@ function _sumFibs(maxFibValue) {
   if (maxFibValue <= 0){
     return undefined;
   }
-  else{
-    while (next <= maxFibValue){
+
+  while (next <= maxFibValue){
+    prev = curr;
+    curr = next;
+    next = prev + curr;
+
+    if (curr % 2 === 0){
+      sum += curr;
+    }
+  }
+  return sum;
+}
+
+
+// bonus round
+function _highestFibonacciNumber (maxFibValue){
+
+  var sum = 0;
+  var prev = 0;
+  var curr = 1;
+  var next = 1;
+
+  if (maxFibValue <= 0){
+    return undefined;
+  } else{
+      while (next <= maxFibValue){
       prev = curr;
       curr = next;
       next = prev + curr;
@@ -26,21 +50,10 @@ function _sumFibs(maxFibValue) {
     }
 
   }
-  return sum;
+  return curr;
+
+
 }
-
-
-// bonus round
-function _highestFibonacciNumber (maxFibValue){
-  var highest = 0;
-
-  //define your base case, validate your input
-
-
-  //do your work here
-
-  return highest;
-};
 
 /**
  * Do not modify code below.
@@ -50,3 +63,24 @@ module.exports = {
   sumFibs : _sumFibs,
   highestFibonacciNumber : _highestFibonacciNumber
 };
+
+
+function _sumFibs(maxFibValue) {
+
+  var Fibs = [0,1];
+  var Total = 0;
+
+  while (Fibs[Fibs.length-1] < maxFibValue){
+      var sum = Fibs[Fibs.length-1] + Fibs[Fibs.length-2];
+      Fibs.push(sum);
+  }
+
+  for (var i=0; i < Fibs.length; i++){
+    if (Fibs[i]%2 === 0){
+      Total += Fibs[i];
+    }
+  }
+
+return Total;
+
+}
